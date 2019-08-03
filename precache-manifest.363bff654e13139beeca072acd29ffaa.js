56358d5c7d4b4d783868ea78927f2dbad463b375
vadymvoitko <vadym.voitko@gmail.com>
2019-08-03 18:35:50 +0300

    f

---
+++ precache-manifest.363bff654e13139beeca072acd29ffaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5640a166f967f2848d03e55db73ebae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04e021927d3f586c81e2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1bf7a9a5e573e06b0eab",
+    "url": "/static/js/2.2675739e.chunk.js"
+  },
+  {
+    "revision": "04e021927d3f586c81e2",
+    "url": "/static/js/main.905048a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
